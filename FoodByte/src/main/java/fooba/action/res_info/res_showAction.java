4bc9b4fc0b53c43300ce72c54de24a3cb4c6bbf2
JAVA01 <JAVA01@DESKTOP-AA65UFV>
2022-12-30 14:30:06 +0900

    show factory

---
+++ FoodByte/src/main/java/fooba/action/res_info/res_showAction.java
@@ -0,0 +1,41 @@
+package fooba.action.res_info;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import fooba.VO.FoodmenuVO;
+import fooba.VO.RestaurantVO;
+import fooba.action.Action;
+import fooba.dao.ResDao;
+
+public class res_showAction implements Action {
+
+	@Override
+	public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		  String url="fooba.do?command=res_menu";
+	      HttpSession session=request.getSession();
+	      RestaurantVO rvo=(RestaurantVO)session.getAttribute("loginUser");
+	      if(rvo==null) {
+	         url="fooba.do?command=res_loginFrom";
+	      }else {
+	    	  
+	    	ResDao rdao=ResDao.getInstance();
+	  		int rseq=Integer.parseInt(request.getParameter("rseq"));
+	  		ArrayList<FoodmenuVO>FoodList=rdao.foodList(rseq);
+	  		
+	  		request.setAttribute("RestaurantVO", rvo);
+	  		request.setAttribute("FoodList", FoodList);
+	  		
+	    	  
+	      }
+	      request.getRequestDispatcher(url).forward(request, response);
+
+
+	}
+
+}
