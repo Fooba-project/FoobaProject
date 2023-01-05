8020e80669a65568069616c20ab14359c4c36c24
Isaac Jeong <isacgood@naver.com>
2023-01-05 17:00:41 +0900

    주문내역 작업중

--- FoodByte/src/main/java/fooba/action/member/memberOrderListAction.java
+++ FoodByte/src/main/java/fooba/action/member/memberOrderListAction.java
@@ -39,30 +39,37 @@ public class memberOrderListAction implements Action {
 	         for (OrderVO ovo : list) { // 현재 주문배송중인 레스토랑수만큼 반복
 	        	 ArrayList<OrderDetailVO> detailList = new ArrayList<>();
 	        	 detailList = odao.getOrderDetailbyOseq(ovo.getOseq());
-	        	 
+	        	 System.out.println("ovo.getOseq() : "+ovo.getOseq());
 	        	 for (OrderDetailVO odvo : detailList) { // 주문한 메뉴별(odseq 수만큼)로 반복
 	        		 FoodDao fdao = FoodDao.getInstance();
 	        		 FoodmenuVO fvo = new FoodmenuVO();
 	        		 fvo = fdao.getFoodDetail(odvo.getFseq());
 	        		 oname = oname+fvo.getFname();
-	        		 
+	        		 System.out.println("fvo.getFname() : "+fvo.getFname());
 	        		 if (odvo.getSideyn1()+odvo.getSideyn2()+odvo.getSideyn3()>0) {
 	        			 oname = oname + "(";
 		        		 if (odvo.getSideyn1()==1) {
 		        			 fvo.setFprice(fvo.getFprice()+fvo.getFsideprice1());
 		        			 oname = oname + fvo.getFside1();
+		        			 if (odvo.getSideyn2()==1 || odvo.getSideyn3()==1) {
+		        				 oname = oname + ", ";
+		        			 }
 		        		 }
 		        		 if (odvo.getSideyn2()==1) {
 		        			 fvo.setFprice(fvo.getFprice()+fvo.getFsideprice2());
 		        			 oname = oname + fvo.getFside2();
+		        			 if (odvo.getSideyn3()==1) {
+		        				 oname = oname + ", ";
+		        			 }
 		        		 }
 		        		 if (odvo.getSideyn3()==1) {
 		        			 fvo.setFprice(fvo.getFprice()+fvo.getFsideprice3());
 		        			 oname = oname + fvo.getFside3();
 		        		 }
-		        		 oname = oname + ") ";
-	        		 } else oname = oname+" ";
+		        		 oname = oname + "), ";
+	        		 } else oname = oname+", ";
 	        		 total = total + (fvo.getFprice()*odvo.getQuantity());
+	        		 System.out.println("oname : "+oname);
 	        		 ovo.setRname(fdao.getRname(fvo.getRseq()));
 	        	 }
 	        	 ovo.setTotalprice(total);
